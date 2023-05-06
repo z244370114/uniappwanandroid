fb5c550391ee4a6aeada2c5bccfbcfabdb187f89
lk-kai <806306842@qq.com>
2023-05-07 00:43:28 +0800

    fix 首页点赞点击事件冒泡

--- components/itemHome.vue
+++ components/itemHome.vue
@@ -19,9 +19,11 @@
 				<u--text :text="item.superChapterName" color="#cccccc" size="12"></u--text>
 			</u-col>
 			<u-col span="1">
-				<u-image width="28px" height="28px"
-					:src="item.zan == 0 ? '/static/ic_collect_no.png':'/static/ic_collect_yes.png'"
-					@click="click"></u-image>
+				<view @click.stop>
+					<u-image width="28px" height="28px"
+						:src="item.zan == 0 ? '/static/ic_collect_no.png':'/static/ic_collect_yes.png'"
+						@click="click"></u-image>
+				</view>
 			</u-col>
 		</u-row>
 		<u-line margin="10px 0 0 0"></u-line>
